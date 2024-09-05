bbf6503272b7c910254f3530e683ad2dd5c55547
rkozhan~ <yalta.vrk@gmail.com>
2024-09-05 15:33:32 +0200

    Deployment init

---
+++ MyBlazorShopHosted/Web/MyBlazorShopHosted.Web.Client/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyBlazorShopHosted.Web.Client;
+using Serilog.Extensions.Logging;
+using Serilog;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var http = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+http.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue
+{
+    NoCache = true
+};
+builder.Services.AddScoped(sp => http);
+
+var levelSwitch = new Serilog.Core.LoggingLevelSwitch();
+Log.Logger = new LoggerConfiguration()
+    .Enrich.WithProperty("InstanceId", Guid.NewGuid().ToString("n"))
+    .WriteTo.BrowserHttp(http)
+    .CreateLogger();
+
+builder.Logging.AddProvider(new SerilogLoggerProvider());
+
+using var response = await http.GetAsync("productlisting.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+
+builder.Configuration.AddJsonStream(stream);
+
+using var environmentResponse = await http.GetAsync("productlisting." + builder.HostEnvironment.Environment + ".json");
+
+if (environmentResponse.IsSuccessStatusCode)
+{
+    using var environmentStream = await environmentResponse.Content.ReadAsStreamAsync();
+    builder.Configuration.AddJsonStream(environmentStream);
+}
+
+builder.Services.AddLocalization();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
+await builder.Build().RunAsync();
---
+++ MyBlazorShopHosted/Web/MyBlazorShopHosted.Web.Server/Program.cs
@@ -0,0 +1,49 @@
+using MyBlazorShopHosted.Libraries.Services.Product;
+using MyBlazorShopHosted.Libraries.Services.ShoppingCart;
+using MyBlazorShopHosted.Libraries.Services.Storage;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// Dependency injection
+builder.Services.AddSingleton<IStorageService, StorageService>();
+builder.Services.AddSingleton<IShoppingCartService, ShoppingCartService>();
+builder.Services.AddTransient<IProductService, ProductService>();
+
+builder.Host.UseSerilog((hbc, lc) => lc.ReadFrom.Configuration(hbc.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseSerilogIngestion();
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
