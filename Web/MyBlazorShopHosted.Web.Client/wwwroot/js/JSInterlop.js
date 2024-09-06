49ed2c903ed64780a074b388a6ef9f55aaf88dbd
rkozhan~ <yalta.vrk@gmail.com>
2024-09-06 15:01:54 +0200

    reference js elements in a Razor component

--- Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlop.js
+++
@@ -1,8 +0,0 @@
-﻿export function GetLocalOffsetMinutes() {
-    var date = new Date();
-
-    return date.getTimezoneOffset();
-}
-export function AddToCart(productName) {
-    alert(productName + " has been added to the cart");
-}
---
+++ Web/MyBlazorShopHosted.Web.Client/wwwroot/js/JSInterlopElement.js
@@ -0,0 +1,11 @@
+﻿export function ToggleElement(element) {
+    if (element == undefined) {
+        return;
+    }
+    if (element.style.display == "none") {
+        element.style.display = "block";
+    }
+    else {
+        element.style.display = "none";
+    }
+}
