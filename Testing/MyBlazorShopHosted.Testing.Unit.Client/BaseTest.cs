6d120cdd83800d60f92b87d194820a36559f22ef
rkozhan~ <yalta.vrk@gmail.com>
2024-09-10 12:04:48 +0200

    Test components initial

---
+++ Testing/MyBlazorShopHosted.Testing.Unit.Client/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using Bunit;
+using Bunit.TestDoubles;
+using Microsoft.Extensions.DependencyInjection;
+using MyBlazorShopHosted.Libraries.Shared.Product.Models;
+using MyBlazorShopHosted.Web.Client.StateManagement;
+using RichardSzalay.MockHttp;
+
+namespace MyBlazorShopHosted.Testing.Unit.Client
+{
+    public abstract class BaseTest : IDisposable
+    {
+        protected readonly TestContext _testContext;
+        public BaseTest()
+        {
+            _testContext = new TestContext();
+            _testContext.AddFakePersistentComponentState();           
+            _testContext.Services.AddSingleton<IShoppingCartStateContainer, ShoppingCartStateContainer>();
+        }
+
+        public void Dispose()
+        {
+            if (_testContext != null)
+            {
+                _testContext.Dispose();
+            }
+        }
+    }
+}
