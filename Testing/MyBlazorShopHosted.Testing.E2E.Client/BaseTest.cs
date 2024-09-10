27738ff273b58be72ebf16bfbb86afb3063909b4
rkozhan~ <yalta.vrk@gmail.com>
2024-09-10 12:57:04 +0200

    E2E testing in Blazor WebAssembly

---
+++ Testing/MyBlazorShopHosted.Testing.E2E.Client/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace MyBlazorShopHosted.Testing.E2E.Client
+{
+    public abstract class BaseTest
+    {
+        protected async Task<IPage> CreateBrowserAsync()
+        {
+            var playwright = await Playwright.CreateAsync();
+          
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Args = new string[] { "--start-maximized" },
+                Headless = false, 
+            });
+
+            var context = await browser.NewContextAsync(new BrowserNewContextOptions()
+            {
+                ViewportSize = ViewportSize.NoViewport
+            });
+          
+            return await context.NewPageAsync();     
+        }
+    }
+}
